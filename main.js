f1e7e4f1c505364919c776fdafa6f44fd9cc16b2
ECastro08 <worldmechatronics2023@gmail.com>
2023-08-21 16:24:42 -0500

    se hizo el detalle de los productos,y se agrego id a las clases repetidas Product-detail

--- main.js
+++ main.js
@@ -3,35 +3,35 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
-menuCarIcon.addEventListener('click', toggleCarrito);
+menuCarIcon.addEventListener('click', toggleCarritoAside);
 
 /* Fusion menu desktop y mobile con el carrito de compras */
 
 function toggleDesktopMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive'); 
+        shoppingCartContainer.classList.add('inactive'); 
       }
       
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive'); 
+        shoppingCartContainer.classList.add('inactive'); 
       }
     mobileMenu.classList.toggle('inactive');
 }
 
-function toggleCarrito() {
+function toggleCarritoAside() {
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
     const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
 
@@ -40,7 +40,7 @@ function toggleCarrito() {
   } else if (!isDesktopMenuClosed) {
     desktopMenu.classList.add('inactive');
   }
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 /* Lista de productos */
